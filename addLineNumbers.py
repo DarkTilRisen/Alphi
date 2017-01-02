8f846f32f682e2ac9d3cad5dcccd95dfd19eac31
tobiah <tobiah@live.be>
2017-01-02 15:25:56 +0100

    final code

--- addLineNumbers.py
+++ addLineNumbers.py
@@ -1,18 +1,23 @@
+#!/usr/bin/env python
+
 import os
+import sys
 
-dirList=os.listdir(".")
-for fname in dirList:
-    print fname
-
-
-
-
-def createName(filename):
-    pass
-def addlines(filename,outputname):
-    with open(filename,"r") as input:
-        with open(outputname,"w") as output:
+def addlines(filename, outputfile):
+    with open(filename) as input:
             linenumber = 0
             for line in input:
-                
+                outputfile.write(str(linenumber) + " " + line)
                 linenumber += 1
+
+if __name__ == '__main__':
+    topdir = sys.argv[1]
+    exten = sys.argv[3]
+    start = True
+    with open(sys.argv[2], "w") as output:
+        for dirpath, dirnames, files in os.walk(topdir):
+            for name in files:
+                if name.lower().endswith(exten):
+                    output.write(("\n\n" if not start else "") + name+"\n\n" )
+                    addlines(os.path.join(dirpath, name),output)
+                    start = False
